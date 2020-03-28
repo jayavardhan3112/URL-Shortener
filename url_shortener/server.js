5fe4f7036797e63705cdc6f0999d946be885dc17
Jayavardhan <jayavardhan3112@gmail.com>
2020-03-28 09:42:57 +0530

    Update server.js

--- url_shortener/server.js
+++ url_shortener/server.js
@@ -12,8 +12,9 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req,res) => {
-	const shortUrls = await ShortUrl.find()
-	res.render('index', {shortUrls: shortUrls})
+	const shorturl = await ShortUrl.find()
+	const url = await ShortUrl.find().sort({_id:-1}).limit(1)
+	res.render('index', {shortUrls: shorturl, url:url[0]})
 })
 
 app.post('/shortUrls', async (req,res) => {
@@ -24,6 +25,7 @@ app.post('/shortUrls', async (req,res) => {
 app.get('/:shortUrl', async (req,res) => {
 	const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
 	if (shortUrl == null) return res.sendStatus(404)
+
 	shortUrl.clicks++
 	shortUrl.save()
 	res.redirect(shortUrl.full)
